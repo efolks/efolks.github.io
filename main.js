d86848d7ad671d54b8ca2225a2228a923aa551e2
Eric Folks <efolks@live.com>
2019-07-11 16:25:48 -0400

    before removing some semantic

--- main.js
+++ main.js
@@ -8,29 +8,39 @@ const gracefulVenuesGitHubLink = document.getElementById('graceful-venues-github
 const gracefulVenuesSiteLink = document.getElementById('graceful-venues-site-link')
 
 sixDegrees.addEventListener('mouseover', function () {
+    sixDegreesLink.style.padding = '20px 0px'
     sixDegreesLink.innerHTML = 'GitHub Repository'
 })
 
 sixDegrees.addEventListener('mouseout', function () {
+    sixDegreesLink.style.padding = '0px'
     sixDegreesLink.innerHTML = ''
 })
 
 scrapBookStudio.addEventListener('mouseover', function () {
+    scrapBookStudioGitHubLink.style.padding = '20px 0px'
+    scrapBookStudioSiteLink.style.padding = '20px 0px'
     scrapBookStudioGitHubLink.innerHTML = 'GitHub Repository'
     scrapBookStudioSiteLink.innerHTML = 'Visit Site'
 })
 
 scrapBookStudio.addEventListener('mouseout', function () {
+    scrapBookStudioGitHubLink.style.padding = '0px'
+    scrapBookStudioSiteLink.style.padding = '0px'
     scrapBookStudioGitHubLink.innerHTML = ''
     scrapBookStudioSiteLink.innerHTML = ''
 })
 
 gracefulVenues.addEventListener('mouseover', function () {
+    gracefulVenuesGitHubLink.style.padding = '20px 0px'
+    gracefulVenuesSiteLink.style.padding = '20px 0px'
     gracefulVenuesGitHubLink.innerHTML = 'GitHub Repository'
     gracefulVenuesSiteLink.innerHTML = 'Visit Site'
 })
 
 gracefulVenues.addEventListener('mouseout', function () {
+    gracefulVenuesGitHubLink.style.padding = '0px'
+    gracefulVenuesSiteLink.style.padding = '0px'
     gracefulVenuesGitHubLink.innerHTML = ''
     gracefulVenuesSiteLink.innerHTML = ''
 })
