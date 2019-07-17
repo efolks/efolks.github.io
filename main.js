d694bea76fde024ae1b910553157ffa0f305f81d
Eric Folks <efolks@live.com>
2019-07-17 17:02:27 -0400

    DOM manipulation removed from cards

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
-const sixDegrees = document.getElementById('six-degrees')
-const sixDegreesLink = document.getElementById('six-degrees-link')
-const scrapBookStudio = document.getElementById('scrapbook-studio')
-const scrapBookStudioGitHubLink = document.getElementById('scrapbook-studio-github-link')
-const scrapBookStudioSiteLink = document.getElementById('scrapbook-studio-site-link')
-const gracefulVenues = document.getElementById('graceful-venues')
-const gracefulVenuesGitHubLink = document.getElementById('graceful-venues-github-link')
-const gracefulVenuesSiteLink = document.getElementById('graceful-venues-site-link')
+// const sixDegrees = document.getElementById('six-degrees')
+// const sixDegreesLink = document.getElementById('six-degrees-link')
+// const scrapBookStudio = document.getElementById('scrapbook-studio')
+// const scrapBookStudioGitHubLink = document.getElementById('scrapbook-studio-github-link')
+// const scrapBookStudioSiteLink = document.getElementById('scrapbook-studio-site-link')
+// const gracefulVenues = document.getElementById('graceful-venues')
+// const gracefulVenuesGitHubLink = document.getElementById('graceful-venues-github-link')
+// const gracefulVenuesSiteLink = document.getElementById('graceful-venues-site-link')
 
-sixDegrees.addEventListener('mouseover', function () {
-    sixDegreesLink.style.padding = '20px 0px'
-    sixDegreesLink.innerHTML = 'GitHub Repository'
-})
+// sixDegrees.addEventListener('mouseover', function () {
+//     sixDegreesLink.style.padding = '20px 0px'
+//     sixDegreesLink.innerHTML = 'GitHub Repository'
+// })
 
-sixDegrees.addEventListener('mouseout', function () {
-    sixDegreesLink.style.padding = '0px'
-    sixDegreesLink.innerHTML = ''
-})
+// sixDegrees.addEventListener('mouseout', function () {
+//     sixDegreesLink.style.padding = '0px'
+//     sixDegreesLink.innerHTML = ''
+// })
 
-scrapBookStudio.addEventListener('mouseover', function () {
-    scrapBookStudioGitHubLink.style.padding = '20px 0px'
-    scrapBookStudioSiteLink.style.padding = '20px 0px'
-    scrapBookStudioGitHubLink.innerHTML = 'GitHub Repository'
-    scrapBookStudioSiteLink.innerHTML = 'Visit Site'
-})
+// scrapBookStudio.addEventListener('mouseover', function () {
+//     scrapBookStudioGitHubLink.style.padding = '20px 0px'
+//     scrapBookStudioSiteLink.style.padding = '20px 0px'
+//     scrapBookStudioGitHubLink.innerHTML = 'GitHub Repository'
+//     scrapBookStudioSiteLink.innerHTML = 'Visit Site'
+// })
 
-scrapBookStudio.addEventListener('mouseout', function () {
-    scrapBookStudioGitHubLink.style.padding = '0px'
-    scrapBookStudioSiteLink.style.padding = '0px'
-    scrapBookStudioGitHubLink.innerHTML = ''
-    scrapBookStudioSiteLink.innerHTML = ''
-})
+// scrapBookStudio.addEventListener('mouseout', function () {
+//     scrapBookStudioGitHubLink.style.padding = '0px'
+//     scrapBookStudioSiteLink.style.padding = '0px'
+//     scrapBookStudioGitHubLink.innerHTML = ''
+//     scrapBookStudioSiteLink.innerHTML = ''
+// })
 
-gracefulVenues.addEventListener('mouseover', function () {
-    gracefulVenuesGitHubLink.style.padding = '20px 0px'
-    gracefulVenuesSiteLink.style.padding = '20px 0px'
-    gracefulVenuesGitHubLink.innerHTML = 'GitHub Repository'
-    gracefulVenuesSiteLink.innerHTML = 'Visit Site'
-})
+// gracefulVenues.addEventListener('mouseover', function () {
+//     gracefulVenuesGitHubLink.style.padding = '20px 0px'
+//     gracefulVenuesSiteLink.style.padding = '20px 0px'
+//     gracefulVenuesGitHubLink.innerHTML = 'GitHub Repository'
+//     gracefulVenuesSiteLink.innerHTML = 'Visit Site'
+// })
 
-gracefulVenues.addEventListener('mouseout', function () {
-    gracefulVenuesGitHubLink.style.padding = '0px'
-    gracefulVenuesSiteLink.style.padding = '0px'
-    gracefulVenuesGitHubLink.innerHTML = ''
-    gracefulVenuesSiteLink.innerHTML = ''
-})
+// gracefulVenues.addEventListener('mouseout', function () {
+//     gracefulVenuesGitHubLink.style.padding = '0px'
+//     gracefulVenuesSiteLink.style.padding = '0px'
+//     gracefulVenuesGitHubLink.innerHTML = ''
+//     gracefulVenuesSiteLink.innerHTML = ''
+// })
